a9c3effcddb02c72252f3edb2a13e3ea4ca62d69
juanesvega-tech <nataliaballs19@gmail.com>
2025-11-20 11:04:31 -0500

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 
 import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
@@ -7,15 +6,23 @@ import ForgotPasswordPage from "./pages/ForgotPage/ForgotPage.jsx";
 import ResetPage from "./pages/ResetPage/ResetPage.jsx";
 import DashboardPage from "./dashboard/DashboardPage.jsx";
 
+// Importar tu CSS global
+import "./styles/global.css";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Página principal */}
         <Route path="/" element={<DashboardPage />} />
+
+        {/* Las 4 vistas que hiciste */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot" element={<ForgotPasswordPage />} />
         <Route path="/reset" element={<ResetPage />} />
+
       </Routes>
     </BrowserRouter>
   );
